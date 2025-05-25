48bc0517657435f2e1192c21a9589073e50d2d30
ChristineKanga-VF <christinek@victoryfarmskenya.com>
2025-05-25 20:55:16 +0300

    fix: bulk upload reports

---
+++ upande_vf_custom/upande_vf_custom/report/international_payments_rwf_bank_bulk_upload/international_payments_rwf_bank_bulk_upload.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2025, Upande Ltd and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["International Payments RWF Bank Bulk Upload"] = {
+	"filters": [
+
+	]
+};
