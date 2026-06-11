d26dc4807556548b30b6d44dd1643c143e5b2f5b
MEryl830 <112549427+MEryl830@users.noreply.github.com>
2026-06-11 22:18:00 +0300

    Modify get_data to include non-standard fieldnames
    Updated the get_data function to include non-standard field names for various document types.

--- upande_vf_custom/upande_vf_custom/doctype/requisition_form/requisition_form_dashboard.py
+++ upande_vf_custom/upande_vf_custom/doctype/requisition_form/requisition_form_dashboard.py
@@ -1,22 +1,30 @@
 from frappe import _
 
-
 def get_data():
-	return {
-		"fieldname": "custom_requisition_form",
-		"internal_links": {
-			"Sales Order": ["items", "sales_order"],
-			"Project": ["items", "project"],
-			"Cost Center": ["items", "cost_center"],
-		},
-		"transactions": [
-			{
-				"label": _("Reference"),
-				"items": ["Sales Order", "Request for Quotation", "Supplier Quotation", "Purchase Order"],
-			},
-			{"label": _("Stock"), "items": ["Stock Entry", "Purchase Receipt", "Pick List"]},
-			{"label": _("Manufacturing"), "items": ["Work Order"]},
-			{"label": _("Internal Transfer"), "items": ["Sales Order"]},
-			{"label": _("Accounting Dimensions"), "items": ["Project", "Cost Center"]},
-		],
-	}
+    return {
+        "fieldname": "custom_requisition_form",
+        "non_standard_fieldnames": {
+            "Purchase Order": "material_request",
+            "Request for Quotation": "material_request",
+            "Supplier Quotation": "material_request",
+            "Purchase Receipt": "material_request",
+            "Pick List": "material_request",
+            "Work Order": "material_request",
+            "Sales Order": "material_request",
+        },
+        "internal_links": {
+            "Sales Order": ["items", "sales_order"],
+            "Project": ["items", "project"],
+            "Cost Center": ["items", "cost_center"],
+        },
+        "transactions": [
+            {
+                "label": _("Reference"),
+                "items": ["Sales Order", "Request for Quotation", "Supplier Quotation", "Purchase Order"],
+            },
+            {"label": _("Stock"), "items": ["Stock Entry", "Purchase Receipt", "Pick List"]},
+            {"label": _("Manufacturing"), "items": ["Work Order"]},
+            {"label": _("Internal Transfer"), "items": ["Sales Order"]},
+            {"label": _("Accounting Dimensions"), "items": ["Project", "Cost Center"]},
+        ],
+    }
