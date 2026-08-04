63c22a155a166a80c8e598a54130efd0ca85d479
MEryl830 <112549427+MEryl830@users.noreply.github.com>
2026-08-04 20:02:45 +0300

    Feat: Project Creation

--- upande_vf_custom/custom_asset_scripts/project.js
+++ upande_vf_custom/custom_asset_scripts/project.js
@@ -1,189 +1,192 @@
 frappe.ui.form.on('Project', {
     refresh: function(frm) {
-        // Add or remove the button based on project type
         manage_create_asset_button(frm);
-        // Calculate and set total project cost when the form is refreshed
         calculate_and_set_custom_total_project_cost(frm);
     },
+
     project_type: function(frm) {
-        // Add or remove the button based on project type change
         manage_create_asset_button(frm);
-        // Recalculate project cost when the project type changes
         calculate_and_set_custom_total_project_cost(frm);
     }
 });
 
 function manage_create_asset_button(frm) {
-    // Check if the project type is "Capital Work in Progress"
-    if (frm.doc.project_type === "Capital Work in Progress") {
-        // Ensure the button is added only once
-        if (!frm.custom_buttons || !frm.custom_buttons['Create Asset']) {
-            frm.page.set_primary_action(__('Create Asset'), function() {
-                if (frm.doc.status !== "Completed") {
-                    frappe.msgprint(__('The project must be marked as completed before creating an asset.'));
-                    return;
-                }
-                frappe.confirm(
-                    'Are you sure you want to create an asset?',
-                    function() {
-                        frappe.prompt([
-                            {
-                                'label': 'Item Code',
-                                'fieldname': 'item_code',
-                                'fieldtype': 'Link',
-                                'options': 'Item',
-                                'reqd': 1,
-                                'description': 'Select an existing Item Code or create a new one.',
-                                'filters': {
-                                    'item_group': 'Fixed Assets' // Filter for fixed assets only
-                                }
-                            },
-                            {
-                                'label': 'Location',
-                                'fieldname': 'location',
-                                'fieldtype': 'Link',
-                                'options': 'Location',
-                                'reqd': 1,
-                                'description': 'Select an existing Location or create a new one.'
-                            }
-                        ],
-                        function(values) {
-                            // Fetch the total project cost
-                            let gross_purchase_amount = frm.doc.custom_total_project_cost || 0;
 
-                            console.log('Gross Purchase Amount:', gross_purchase_amount);
-                            // Check if gross_purchase_amount is valid
-                            if (gross_purchase_amount <= 0) {
-                                frappe.msgprint(__('Total Project Cost cannot be zero. Please ensure there are valid expenses linked to the project.'));
-                                return;
-                            }
+    // Remove existing button first to avoid duplicates
+    frm.remove_custom_button(__('Create Asset'));
 
-                            // Check if asset already exists
-                            frappe.call({
-                                method: "frappe.client.get_list",
-                                args: {
-                                    doctype: "Asset",
-                                    filters: {
-                                        "asset_name": frm.doc.project_name,
-                                        "item_code": values.item_code,
-                                        "company": frm.doc.company
-                                    },
-                                    fields: ["name"]
+    // Only show button for saved CWIP Projects
+    if (!frm.is_new() &&
+        frm.doc.project_type === "Capital Work in Progress") {
+
+        frm.add_custom_button(__('Create Asset'), function() {
+
+            if (frm.doc.status !== "Completed") {
+                frappe.msgprint(__('The project must be marked as completed before creating an asset.'));
+                return;
+            }
+
+            frappe.confirm(
+                __('Are you sure you want to create an asset?'),
+                function() {
+
+                    frappe.prompt([
+                        {
+                            label: 'Item Code',
+                            fieldname: 'item_code',
+                            fieldtype: 'Link',
+                            options: 'Item',
+                            reqd: 1,
+                            description: 'Select an existing Item Code.',
+                            filters: {
+                                item_group: 'Fixed Assets'
+                            }
+                        },
+                        {
+                            label: 'Location',
+                            fieldname: 'location',
+                            fieldtype: 'Link',
+                            options: 'Location',
+                            reqd: 1
+                        }
+                    ],
+                    function(values) {
+
+                        let gross_purchase_amount = frm.doc.custom_total_project_cost || 0;
+
+                        if (gross_purchase_amount <= 0) {
+                            frappe.msgprint(__('Total Project Cost cannot be zero.'));
+                            return;
+                        }
+
+                        // Check if Asset already exists
+                        frappe.call({
+                            method: "frappe.client.get_list",
+                            args: {
+                                doctype: "Asset",
+                                filters: {
+                                    asset_name: frm.doc.project_name,
+                                    item_code: values.item_code,
+                                    company: frm.doc.company
                                 },
-                                callback: function(response) {
-                                    if (response.message && response.message.length > 0) {
-                                        frappe.msgprint(__('An asset already exists.'));
-                                    } else {
-                                        // Create Asset
+                                fields: ["name"]
+                            },
+                            callback: function(r) {
+
+                                if (r.message && r.message.length) {
+                                    frappe.msgprint(__('An Asset already exists.'));
+                                    return;
+                                }
+
+                                // Create Asset
+                                frappe.call({
+                                    method: "frappe.client.insert",
+                                    args: {
+                                        doc: {
+                                            doctype: "Asset",
+                                            item_code: values.item_code,
+                                            asset_name: frm.doc.project_name,
+                                            purchase_date: frm.doc.expected_end_date || frappe.datetime.get_today(),
+                                            gross_purchase_amount: gross_purchase_amount,
+                                            custom_project_: frm.doc.name,
+                                            cost_center: frm.doc.cost_center,
+                                            company: frm.doc.company,
+                                            is_existing_asset: 1,
+                                            location: values.location
+                                        }
+                                    },
+                                    callback: function(asset) {
+
+                                        if (!asset.message) return;
+
+                                        let asset_name = asset.message.name;
+
+                                        frappe.msgprint(
+                                            __('Asset {0} created successfully.', [asset_name])
+                                        );
+
+                                        // Create Journal Entry
                                         frappe.call({
                                             method: "frappe.client.insert",
                                             args: {
                                                 doc: {
-                                                    "doctype": "Asset",
-                                                    "item_code": values.item_code,
-                                                    "asset_name": frm.doc.project_name,
-                                                    "purchase_date": frm.doc.expected_end_date || frappe.datetime.get_today(),
-                                                    "gross_purchase_amount": gross_purchase_amount,
-                                                    "custom_project_": frm.doc.name,
-                                                    "cost_center": frm.doc.cost_center,
-                                                    "company": frm.doc.company,
-                                                    "is_existing_asset": 1,
-                                                    "location": values.location
+                                                    doctype: "Journal Entry",
+                                                    posting_date: frappe.datetime.get_today(),
+                                                    company: frm.doc.company,
+                                                    project: frm.doc.name,
+                                                    accounts: [
+                                                        {
+                                                            account: "CWIP Account - VF",
+                                                            credit_in_account_currency: gross_purchase_amount
+                                                        },
+                                                        {
+                                                            account: "Buildings - VF",
+                                                            debit_in_account_currency: gross_purchase_amount,
+                                                            reference_type: "Asset",
+                                                            reference_name: asset_name,
+                                                            project: frm.doc.name
+                                                        }
+                                                    ]
                                                 }
                                             },
-                                            callback: function(response) {
-                                                if (response.message) {
-                                                    let created_asset_name = response.message.name;  // Capture the asset name
-                                                    frappe.msgprint(__('Asset {0} created successfully.', [created_asset_name]));
+                                            callback: function(je) {
 
-                                                    // Create journal entry (in draft form)
-                                                    frappe.call({
-                                                        method: "frappe.client.insert",
-                                                        args: {
-                                                            doc: {
-                                                                "doctype": "Journal Entry",
-                                                                "posting_date": frappe.datetime.get_today(),
-                                                                "company": frm.doc.company,
-                                                                "accounts": [
-                                                                    {
-                                                                        "account": "CWIP Account - VF",  // Replace with your CWIP account
-                                                                        "credit_in_account_currency": gross_purchase_amount
-                                                                    },
-                                                                    {
-                                                                        "account": "Buildings - VF",  // Replace with your Asset account
-                                                                        "debit_in_account_currency": gross_purchase_amount,
-                                                                        "reference_type": "Asset",
-                                                                        "reference_name": created_asset_name,
-                                                                        "project": frm.doc.name
-                                                                    }
-                                                                ],
-                                                                "project": frm.doc.name
-                                                            }
-                                                        },
-                                                        callback: function(response) {
-                                                            console.log('Journal Entry Callback Executed');
-                                                            if (response.message) {
-                                                                console.log('Journal Entry Created:', response.message.name);
-                                                                frappe.msgprint(__('Journal Entry {0} created successfully.', [response.message.name]));
-                                                            } else {
-                                                                console.log('Journal Entry Creation Failed');
-                                                                frappe.msgprint(__('An error occurred while creating the Journal Entry.'));
-                                                            }
-                                                        }
-                                                    });
+                                                if (je.message) {
+                                                    frappe.msgprint(
+                                                        __('Journal Entry {0} created successfully.', [je.message.name])
+                                                    );
                                                 }
                                             }
                                         });
+
                                     }
-                                }
-                            });
-                        },
-                        'Select Item Code and Location',
-                        'Create');
-                    }
-                );
-            }).addClass('btn-primary');
-        }
-    } else {
-        // Remove the button if project type is not CWIP
-        if (frm.custom_buttons && frm.custom_buttons['Create Asset']) {
-            frm.page.remove_action('Create Asset');
-        }
+                                });
+
+                            }
+                        });
+
+                    },
+                    __('Create Asset'),
+                    __('Create'));
+
+                }
+            );
+
+        }, __('Actions'));
     }
 }
 
 function calculate_and_set_custom_total_project_cost(frm) {
-    if (frm.doc.project_type === "Capital Work in Progress") {
-        // Fetch individual GL entries for the CWIP account and project
-        frappe.call({
-            method: "frappe.client.get_list",
-            args: {
-                doctype: "GL Entry",
-                filters: {
-                    project: frm.doc.name,
-                    account: "410010 - Capital Work in Progress - VFL"  // Replace with your CWIP account
-                },
-                fields: ["debit_in_account_currency", "credit_in_account_currency"]
+
+    if (frm.doc.project_type !== "Capital Work in Progress")
+        return;
+
+    frappe.call({
+        method: "frappe.client.get_list",
+        args: {
+            doctype: "GL Entry",
+            filters: {
+                project: frm.doc.name,
+                account: "410010 - Capital Work in Progress - VFL"
             },
-            callback: function(response) {
-                if (response.message) {
-                    console.log(response.message);
-                    let custom_total_project_cost = 0;
-                    let acc_bal = response.message;
-                    console.log(acc_bal);
-                    
-                    acc_bal.forEach(entry => {
-                        console.log(entry);
-                        custom_total_project_cost += (entry.debit_in_account_currency || 0) - (entry.credit_in_account_currency || 0);
-                    });
-                    
-                    console.log('Total Project Cost:', custom_total_project_cost); // Debugging line
-                    
-                    frm.set_value('custom_total_project_cost', custom_total_project_cost);
-                } else {
-                    frm.set_value('custom_total_project_cost', 0);
-                }
+            fields: [
+                "debit_in_account_currency",
+                "credit_in_account_currency"
+            ]
+        },
+        callback: function(r) {
+
+            let total = 0;
+
+            if (r.message) {
+                r.message.forEach(function(entry) {
+                    total +=
+                        (entry.debit_in_account_currency || 0) -
+                        (entry.credit_in_account_currency || 0);
+                });
             }
-        });
-    }
+
+            frm.set_value("custom_total_project_cost", total);
+        }
+    });
 }
